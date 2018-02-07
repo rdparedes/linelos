19a4e8af5033456d3a7f6ba0900e5525f770115e
Roberto Paredes <rparedes@thoughtworks.com>
2018-02-07 17:46:20 -0300

    skips functional test until it makes sense

--- test/functional/linelos/api/handler_functional_test.clj
+++ test/functional/linelos/api/handler_functional_test.clj
@@ -4,19 +4,16 @@
             [helpers.server :as server]
             [clj-http.client :as client]))
 
-(def ^:private dev-server-port 3333)
+(def ^:private dev-server-port 3478)
 
 (against-background
  [(before :contents (server/start-server dev-server-port))
   (after :contents (server/stop-server))]
  (facts
   "about transactions" :functional
-  (fact "it returns a list of transactions given a Gmail search query"
+  (future-fact "it returns a list of transactions given a Gmail search query"
         (let [api-url           (str "http://localhost:" dev-server-port "/transactions")
-              search-query      "from:(notificaciones@infopacificard.com.ec) pacificard: consumos"
-              query-params      {"query" search-query}
-              response          (client/get api-url {:accept :json :query-params query-params})
-              first-transaction (first (:transacciones (parse-string (response :body) true)))]
+              response          (client/get api-url {:accept :json})]
           (response :status) => 200
           (first-transaction :tienda) => (contains #"[\w\-\s]+")
           (first-transaction :monto) => float?
