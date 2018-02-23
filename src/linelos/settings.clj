b1cc4136a9b16eff1bf0db93ef1c808ac8a8a384
Roberto Paredes <rparedes@thoughtworks.com>
2018-02-23 07:49:28 -0300

    adds env variables

--- src/linelos/settings.clj
+++ src/linelos/settings.clj
@@ -3,6 +3,7 @@
 
 (def app-name "linelos")
 (def app-url (get env :app-url))
+(def frontend-app-url (get env :frontend-app-url))
 (def app-default-port (get env :app-default-port))
 (def email-address (get env :client-email))
 (def gmail-secret-path (get env :gmail-secret-path))
